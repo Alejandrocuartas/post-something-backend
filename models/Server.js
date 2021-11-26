1c2cde75df486f3af2b60ebfd7c340811196c99c
Alejandro Cuartas <cuartasmarin734@gmail.com>
2021-11-26 16:49:11 -0500

    second commit

--- models/Server.js
+++ models/Server.js
@@ -19,6 +19,7 @@ class Server {
         dbConector()
     }
     middlewares(){
+        this.app.use( express.static('public') )
         this.app.use( cors() )
         this.app.use( express.json() )
     }
